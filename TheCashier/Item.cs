d626f79147c033a6c3594a7ed4bef7050ff4ba85
Muhammad Kholif Arrahman <Muhammad.2799@students.amikom.ac.id>
2020-10-21 22:04:52 +0700

    Add project files.

---
+++ TheCashier/Item.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCashier
+{
+    class Item
+    {
+        private int id;
+
+        public string title { get; set; }
+
+        public int quantity { get; set; }
+
+        public double price { get; set; }
+
+        public double subtotal { get; set; }
+
+        public string type;
+        
+
+        public Item(int id, string title, int quantity, double price, string type)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = 0;
+            this.type = type;
+
+        }
+
+      
+ 
+
+        public string getTitle()
+        {
+            return title;
+        }
+
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public double getSubTotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+    }
+}
